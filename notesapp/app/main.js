e426b5a3b1c004693924e690de486f067e4ee683
youxingzhi <youxingzhi@qq.com>
2017-12-29 18:10:02 +0800

    保存文件，拖拽

--- notesapp/app/main.js
+++ notesapp/app/main.js
@@ -1,36 +1,136 @@
-const {app, BrowserWindow, dialog} = require('electron')
-const fs = require('fs')
-
-let mainWindow = null
-
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
+ 
+const windows = new Set();
+const openFiles = new Map();
+ 
 app.on('ready', () => {
-  console.log('Hello from Electron')
-  mainWindow = new BrowserWindow({ show: false })
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-    getFileFromUser()
-  })
-
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
-})
-
-
-const getFileFromUser = exports.getFileFromUser = () => {
-  const files = dialog.showOpenDialog({
+  createWindow();
+});
+ 
+app.on('window-all-closed', () => {
+  if (process.platform === 'darwin') {
+    return false;
+  }
+});
+ 
+app.on('activate', (event, hasVisibleWindows) => {
+  if (!hasVisibleWindows) { createWindow(); }
+});
+ 
+const createWindow = exports.createWindow = () => {
+  let x, y;
+ 
+  const currentWindow = BrowserWindow.getFocusedWindow();
+ 
+  if (currentWindow) {
+    const [ currentWindowX, currentWindowY ] = currentWindow.getPosition();
+    x = currentWindowX + 10;
+    y = currentWindowY + 10;
+  }
+ 
+  let newWindow = new BrowserWindow({ x, y, show: false });
+ 
+  newWindow.loadURL(`file://${__dirname}/index.html`);
+ 
+  newWindow.once('ready-to-show', () => {
+    newWindow.show();
+  });
+ 
+  newWindow.on('close', (event) => {
+    if (newWindow.isDocumentEdited()) {
+      event.preventDefault();
+ 
+      const result = dialog.showMessageBox(newWindow, {
+        type: 'warning',
+        title: 'Quit with Unsaved Changes?',
+        message: 'Your changes will be lost permanently if you do not save.',
+        buttons: [
+          'Quit Anyway',
+          'Cancel',
+        ],
+        cancelId: 1,
+        defaultId: 0
+      });
+ 
+      if (result === 0) newWindow.destroy();
+    }
+  });
+ 
+  newWindow.on('closed', () => {
+    windows.delete(newWindow);
+    newWindow = null;
+  });
+ 
+  return newWindow;
+};
+ 
+const getFileFromUser  = exports.getFileFromUser = (targetWindow) => {
+  const files = dialog.showOpenDialog(targetWindow, {
     properties: ['openFile'],
     filters: [
       { name: 'Text Files', extensions: ['txt'] },
       { name: 'Markdown Files', extensions: ['md', 'markdown'] }
     ]
-  })
-  if (files) { openFile(files[0])}
-}
-
-const openFile = (file) => {
-  const content = fs.readFileSync(file).toString()
-  mainWindow.webContents.send('file-opened', file, content)
-} 
+  });
+ 
+  if (files) { openFile(targetWindow, files[0]); }
+};
+ 
+const openFile = exports.openFile = (targetWindow, file) => {
+  const content = fs.readFileSync(file).toString();
+  startWatchingFile(targetWindow, file);
+  app.addRecentDocument(file);
+  targetWindow.setRepresentedFilename(file);
+  targetWindow.webContents.send('file-opened', file, content);
+};
+ 
+ 
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+  if (!file) {
+    file = dialog.showSaveDialog(targetWindow, {
+      title: 'Save Markdown',
+      defaultPath: app.getPath('documents'),
+      filters: [
+        { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+      ]
+    });
+  }
+ 
+  if (!file) return;
+ 
+  fs.writeFileSync(file, content);
+  openFile(targetWindow, file);
+};
+ 
+const saveHtml = exports.saveHtml = (targetWindow, content) => {
+  const file = dialog.showSaveDialog(targetWindow, {
+    title: 'Save HTML',
+    defaultPath: app.getPath('documents'),
+    filters: [
+      { name: 'HTML Files', extensions: ['html', 'htm'] }
+    ]
+  });
+ 
+  if (!file) return;
+ 
+  fs.writeFileSync(file, content);
+};
+ 
+const startWatchingFile = (targetWindow, file) => {
+  stopWatchingFile(targetWindow);
+ 
+  const watcher = fs.watchFile(file, () => {
+    const content = fs.readFileSync(file);
+    targetWindow.webContents.send('file-changed', file, content);
+  });
+ 
+  openFiles.set(targetWindow, watcher);
+};
+ 
+const stopWatchingFile = (targetWindow) => {
+  if (openFiles.has(targetWindow)) {
+    openFiles.get(targetWindow).close();
+    openFiles.delete(targetWindow);
+  }
+};
--- notesapp/app/renderer.js
+++ notesapp/app/renderer.js
@@ -1,6 +1,8 @@
 const marked = require('marked')
+const path = require('path')
 const { remote, ipcRenderer } = require('electron')
 const mainProcess = remote.require('./main.js');
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -12,6 +14,21 @@ const saveHtmlButton = document.querySelector('#save-html');
 const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
+let filePath = null
+let originalContent = ''
+
+const updateUserInterface = (isEdited) => {
+  let title = 'Fire Sale';
+ 
+  if (filePath) { title = `${path.basename(filePath)} - ${title}`; }
+  if (isEdited) { title = `${title} (Edited)`; }
+ 
+  currentWindow.setTitle(title);
+  currentWindow.setDocumentEdited(isEdited);
+
+  saveMarkdownButton.disabled = !isEdited
+  revertButton.disabled = !isEdited
+};
 
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
@@ -20,13 +37,78 @@ const renderMarkdownToHtml = (markdown) => {
 markdownView.addEventListener('keyup', (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
+  updateUserInterface(currentContent !== originalContent)
 });
 
 openFileButton.addEventListener('click', () => {
-  mainProcess.getFileFromUser();
+  mainProcess.getFileFromUser(currentWindow);
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+  console.log(file)
+  filePath = file;
+  originalContent = content;
+ 
   markdownView.value = content;
   renderMarkdownToHtml(content);
+
+  updateUserInterface(false);
 });
+
+newFileButton.addEventListener('click', () => {
+  mainProcess.createWindow();
+});
+
+saveHtmlButton.addEventListener('click', () => {
+  mainProcess.saveHtml(currentWindow, htmlView.innerHTML);
+});
+
+saveMarkdownButton.addEventListener('click', () => {
+  mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value);
+});
+
+revertButton.addEventListener('click', () => {
+  markdownView.value = originalContent;
+  renderMarkdownToHtml(originalContent);
+});
+
+// 拖拽
+const getDraggedFile = (event) => event.dataTransfer.files[0];
+ 
+const fileTypeIsSupported = (file) => {
+  return ['text/plain', 'text/markdown'].includes(file.type);
+};
+
+markdownView.addEventListener('dragover', (event) => {
+  // const file = getDraggedFile(event);
+ 
+  // if (fileTypeIsSupported(file)) {
+  markdownView.classList.add('drag-over');
+  // } else {
+    // markdownView.classList.add('drag-error');
+  // }
+});
+
+markdownView.addEventListener('drop', (event) => {
+  const file = getDraggedFile(event);
+ 
+  // if (fileTypeIsSupported(file)) {
+  mainProcess.openFile(currentWindow, file.path);
+  // } else {
+    // alert('That file type is not supported');
+  // }
+ 
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
+});
+ 
+
+markdownView.addEventListener('dragleave', () => {
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
+});
+
+document.addEventListener('dragstart', event => event.preventDefault());
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('dragleave', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
